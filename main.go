d41aa5be28a845dc3e74dfe7c297766734eb68b4
Ivan Portugal <ivanbportugal@gmail.com>
2016-08-03 00:46:57 -0500

    finished sever side http endpoints

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	// "encoding/json"
 	"fmt"
 	"github.com/boltdb/bolt"
+	"github.com/gorilla/mux"
+	"io"
 	"log"
+	"net/http"
 	"strings"
 	"time"
 )
+
+var db *bolt.DB
+
+// type stanza struct {
+// 	book      string
+// 	reference string
+// 	verse     string
+// }
 
 func main() {
 
@@ -16,16 +28,37 @@ func main() {
 	kjvDb := "kjv.db"
 
 	// Open DB
-	db, err := bolt.Open(dbsLocation+kjvDb, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	var err error
+	db, err = bolt.Open(dbsLocation+kjvDb, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	query := "love"
+	// Debug
+	// query := "love"
+	// results := queryDb(query, db)
+	// fmt.Printf("results %d \n", len(results))
 
-	results := queryDb(query, db)
-	fmt.Printf("results %d \n", len(results))
+	// Begin Mux
+	r := mux.NewRouter()
+	r.HandleFunc("/search", SearchHandler).Methods("POST")
+	http.Handle("/", r)
+
+	errHttp := http.ListenAndServe(":8080", r)
+	if errHttp != nil {
+		log.Println("Could not launch HTTP server: " + errHttp.Error())
+	}
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+
+	val := r.URL.Query().Get("query")
+	results := queryDb(val, db)
+
+	w.Header().Set("Content-Type", "application/json")
+	toReturn := "[" + strings.Join(results, ",") + "]"
+	io.WriteString(w, toReturn)
 }
 
 func queryDb(query string, db *bolt.DB) []string {
@@ -53,9 +86,10 @@ func queryBucket(bucket *bolt.Bucket, book string, query string) []string {
 	results := []string{}
 	c := bucket.Cursor()
 	for reference, verseText := c.First(); reference != nil; reference, verseText = c.Next() {
-		if CaseInsensitiveContains(string(reference[:]), query) || CaseInsensitiveContains(string(verseText[:]), query) {
+		if CaseInsensitiveContains(book, query) || CaseInsensitiveContains(string(verseText[:]), query) || CaseInsensitiveContains(string(reference[:]), query) {
 			// fmt.Printf("%s %s %s\n", book, reference, verseText)
-			result := fmt.Sprintf("%s %s %s", book, reference, verseText)
+			// j, _ := json.Marshal(stanza{book: book, reference: string(reference), verse: string(verseText)})
+			result := fmt.Sprintf("{\"book\":\"%s\",\"reference\":\"%s\",\"verse\":\"%s\"}", book, string(reference), string(verseText))
 			results = append(results, result)
 		}
 	}
